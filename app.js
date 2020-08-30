c7293a8f379bf6b79327055750b6327cb6eb7f84
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-08-30 15:48:05 -0500

    JS completed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let store = {
       correctAnswer: 'Hot Dog On A Stick'
     },
     {
-      question: 'Which character is not part of the Scooby Gang',
+      question: 'Which character is not part of the Scooby Gang?',
       answers: [
         'Xander',
         'Willow',
@@ -55,12 +55,11 @@ let store = {
       correctAnswer: 'Dawn'
     }
   ],
-  quizStarted: false,
   questionNumber: 0,
   score: 0
 };
 
-let resultsStringText = "";
+let resultsStringText = ""; //string declared globaly so it can be used in multiple functions. 
 
 /**
  * 
@@ -77,17 +76,45 @@ let resultsStringText = "";
  * 
  */
 
+function questionCounter() { //+1 to questionNumber each time called 
+  store.questionNumber += 1;
+}
+
+function scoreCounter() { //+1 to score each time called
+  store.score += 1;
+}
+
+function scorePercentage() { //determines score as a percentage
+  if (store.questionNumber === 0) {
+    return `
+      no score yet!`
+  } else {
+    return `
+      ${((store.score / store.questionNumber) * 100).toFixed(2)}%`;
+  }
+}
+
+function checkAnswer(e) { //this function checks if the user submitted answer is correct
+
+  e.preventDefault();
+  let selectedAnswer = $("input[name='answer']:checked").val(); //gets value from selected radio
+  let currentCorrectAnswer = store.questions[store.questionNumber].correctAnswer; /*gets correct
+  answer from store*/
+
+  /* if/else statement assigns appropriate string based on user selection */
+  if (selectedAnswer === currentCorrectAnswer) {
+    scoreCounter();
+    resultsStringText = `Your answer is correct! You are a true Vampire Slayer!`;
+  } else {
+    resultsStringText = `That is incorrect. The correct answer is ${currentCorrectAnswer}.`;
+  }
+}
+
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
 
-//question counter function increases QuestionNumber eash time questionCounter is called. 
-function questionCounter() {
-  store.questionNumber+=1;
-}
-
-//this is a template for the opening 'splash' page. 
-function generateSplash() {
+function generateSplash() { //this is a template for the opening 'splash' page. 
   return `
   <div class="splash">
     <form>
@@ -97,34 +124,47 @@ function generateSplash() {
       <button type="submit" id="beginQuiz" autofocus> Begin Quiz</button>
     </form>
   </div>
-  `;  
+  `;
 }
 
-function finalScreen() {
+function answerFeedback() { //this is a template that provides feedback based on users answer 
+  return `
+  <div class="feedback">
+    <form>
+      <p>${resultsStringText}</p>
+      <button type="submit" id="next" autofocus> Next Question</button>
+    </form>  
+  </div>    
+  `
+}
+
+function finalScreen() { //this is a template for the final screen
   return `
   <div class="final">
   <form>
     <p>
       Congratulations! You have completed the quiz! <br>
-      Your score is ${store.score}.
+      Your final score is ${scorePercentage()}.
     </p>
-    <button type="submit" id="beginQuiz" autofocus> Restart Quiz</button>
+    <button type="submit" id="restart" autofocus> Restart Quiz</button>
   </form>
 </div>
-`; 
+`;
 }
 
-//a template that generates question and answer strings
-function generateQuestion() {
-  let answerString  = "";
+
+/********** RENDER FUNCTION(S) **********/
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+
+function generateQuestion() { //this function is both a template for the questions, and renders questions based on store state. 
+  let answerString = "";
   let questionObject = store.questions[store.questionNumber];
   let questionText = questionObject.question;
 
-  console.log(store.questionNumber);
-
-  questionObject.answers.map((a, i)=>{ /*using .map to iterate through avalable answers and make a
+  questionObject.answers.map((a, i) => { /*using .map to iterate through avalable answers and make a
     string for each answer*/
-    answerString+= `
+    answerString += `
     <li>
       <input type="radio" name="answer" id="answer-${i}" data-answer="${a}" value="${a}">
       <label for="answer-${i}"> ${a}</label>
@@ -135,7 +175,8 @@ function generateQuestion() {
   <div class="question">
     <form>
       <p>
-        Question ${store.questionNumber+1}
+        Question ${store.questionNumber + 1} out of ${store.questions.length}.
+        Current score: ${scorePercentage()}.
       <p>
         ${questionText}
       </p>
@@ -149,92 +190,64 @@ function generateQuestion() {
   </div>`
 }
 
-function answerFeedback() {
-  return  `
-  <div class="feedback">
-    <form>
-      <p>${resultsStringText}</p>
-      <button type="submit" id="next" autofocus> Next Question</button>
-    </form>  
-  </div>    
-  `
-}
-//this function checks if the user submitted answer is correct
-function checkAnswer(e) {
-
-
-  e.preventDefault(); 
-  let selectedAnswer = $("input[name='answer']:checked").val(); //gets value from selected radio
-  let currentCorrectAnswer = store.questions[store.questionNumber].correctAnswer; /*gets correct
-  answer from store*/
-
-  /*for if/else statement assigns appropriate string based on user selection*/
-  if (selectedAnswer === currentCorrectAnswer) {
-    resultsStringText = `Your answer is correct! You are a true Vampire Slayer!`;
-  } else {
-    resultsStringText = `That is incorrect. The correct answer is ${currentCorrectAnswer}.`;
-  }
-  console.log(resultsStringText);
-}
-
-//on click it has to generate answer feedback
-
-function generateFeedback() {
-  //let feedbackString = answerFeedback(); 
-  console.l
-  console.log(store.questionNumber+1);
-  console.log(store.questions.length); 
-  $('main').on('click', '#ans-button',(event) => {
-    event.preventDefault();
-    $('main').html(answerFeedback()); 
-    // $('#ans-button').prop("id", "restart-quiz");
-    //if (store.questionNumber+1 === store.questions.length) {
-    //  $('#ans-button').attr("id", "restart-quiz");
-    //  $('#restart-quiz').text("Restart Quiz"); 
-  });
-}
-
-
-
-//function that determines if it is the last question, and generates html telling user final score and gives option to restart quiz
-
-//function that keeeps track of score. Correct: 1/5 2/5 3/5 4/5 
-
-/********** RENDER FUNCTION(S) **********/
-
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
-
-function renderQuestion() {
-  $('main').on('click', '#next', (event) => {
-    event.preventDefault();
-    questionCounter();
-    $('main').html(generateQuestion()); 
-}); 
-}
-
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
 
-
-function handleStart() {
-  console.log("handleStart");
-  $(document).ready(function() {
+function handleStart() { 
+  $(document).ready(function () {
     $('main').html(generateSplash());
     $('main').on('click', '#beginQuiz', (event) => {
       event.preventDefault();
-      $('main').html(generateQuestion()); 
-  }); 
-    $('main').html(generateSplash());
-});
+      $('main').html(generateQuestion());
+    });
+  });
 }
-   
+
+function renderQuestion() {
+  $('main').on('click', '#next', (event) => {
+    event.preventDefault();
+    $('main').html(generateQuestion());
+  });
+}
+
+function generateFeedback() {
+  $('main').on('click', '#ans-button', (event) => {
+    event.preventDefault();
+    questionCounter();
+    $('main').html(answerFeedback());
+    if (store.questionNumber === store.questions.length) {
+      $('#next').prop("id", "finish-quiz");
+      $("#finish-quiz").text("Finish");
+    }
+  });
+}
+
+function generateFinal() {
+  $('main').on('click', '#finish-quiz', (event) => {
+    event.preventDefault();
+    $('main').html(finalScreen());
+  });
+}
+
+function restartQuiz() {
+  $('main').on('click', '#restart', (event) => {
+    event.preventDefault();
+    store.questionNumber = 0;
+    store.score = 0;
+    $('main').html(generateQuestion());
+  })
+}
+
+
 function handleQuizApp() {
   generateSplash();
   handleStart();
   renderQuestion();
-  generateQuestion(); 
+  generateQuestion();
   generateFeedback();
-};  
+  generateFinal();
+  restartQuiz()
+};
 
 handleQuizApp(); 
